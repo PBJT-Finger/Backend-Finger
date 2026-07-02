7df52b18f58c7687ec4695641476b88c6ae30445
Melinda9 <melindaeviyanti@anda.com>
2026-07-02 19:53:25 +0700

    fix(build): remove diagnostic scripts causing docker tsc build failure

--- scripts/check-admins.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-    const admins = await prisma.admins.findMany();
-    console.log("Admins:", admins.map(a => ({ id: a.id, username: a.username, email: a.email, name: a.full_name })));
-}
-
-main().finally(() => prisma.$disconnect());
